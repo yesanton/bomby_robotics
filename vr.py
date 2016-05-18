c0488c3b2a6885eb9ffc1199fefc1d22229f8927
aqeel13932 <aqeel.labash@gmail.com>
2016-05-18 15:48:15 +0000

    voice recognition initialization file

---
+++ vr.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# NOTE: this example requires PyAudio because it uses the Microphone class
+
+import speech_recognition as sr
+from time import time
+from threading import Thread
+def ReadAndInterpret():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    t = time()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.record(source,3)
+    print time()-t
+    print ("Stopped Listning")
+    print (type(audio))
+    '''
+    # recognize speech using Sphinx
+    try:
+        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+    '''
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    # recognize speech using Wit.ai
+    WIT_AI_KEY ="RHGDIA7UYYCVUILELWSVOEJAR2C64JJE"# "INSERT WIT.AI API KEY HERE" # Wit.ai keys are 32-character uppercase alphanumeric strings
+    try:
+        print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
+    except sr.UnknownValueError:
+        print("Wit.ai could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Wit.ai service; {0}".format(e))
+
+t1 = Thread(target = ReadAndInterpret)
+#t2 = Thread(target = ReadAndInterpret)
+t1.start()
+#t2.start()
